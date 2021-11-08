49d37d665f01cc5d3ea836dd0cf54c71de8ba3b1
TrevORtega <38680575+TrevORtega@users.noreply.github.com>
2021-11-07 18:32:00 -0800

    Functional code snippets.

--- src/components/submissionModal/snippets.js
+++ src/components/submissionModal/snippets.js
@@ -1,13 +1,184 @@
-import { Container, Row } from 'react-bootstrap';
+import { useState } from 'react';
+import { Col, Container, Dropdown, Row, Form } from 'react-bootstrap';
+import { CopyBlock, dracula } from 'react-code-blocks';
+import styled from 'styled-components';
 
+const StyledInputCol = styled(Col)`
+    overflow-y: scroll;
+`;
+
+const LanguageDropdown = ({snippetValues, setSnippetValues}) => {
+    const { language } = snippetValues;
+
+    const onChange = (eventkey, e) => {
+        e.preventDefault();
+        setSnippetValues({...snippetValues, language: e.target.name});
+    }
+
+    const Items = languages.map(lang => {
+        return <Dropdown.Item name={lang} key={lang}>{lang}</Dropdown.Item>;
+    });
+
+    return (
+        <Dropdown onSelect={onChange}>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                {language} 
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+                {Items}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
+
+const CodeForm = ({snippetValues, setSnippetValues}) => {
+
+    const onChange = (e) => {
+        e.preventDefault();
+        setSnippetValues({...snippetValues, code: e.target.value});
+    }
+
+
+    return (
+        <Form.Group className="mb-3" >
+            <Form.Control 
+                name="code" 
+                as="textarea" 
+                rows={5} 
+                onChange={onChange}
+            />
+        </Form.Group>
+    );
+}
 
 export const Snippets = () => {
-    const mkdown = `#Code`;
+    const [snippetValues, setSnippetValues] = useState({
+        code: '',
+        language: 'Choose Language'
+    })
+
+    const defaultLangauge = 'text';
+    const { code, language } = snippetValues;
+
     return (
         <Container fluid>
-            <Row>Code</Row>
             <Row>
+                <Col>
+                    <h3>
+                        Enter a Code Snippet!
+                    </h3>
+                </Col>
+            </Row>
+            <Row>
+                <StyledInputCol>
+                    <LanguageDropdown 
+                        snippetValues={snippetValues}
+                        setSnippetValues={setSnippetValues}
+                    />
+                    <CodeForm 
+                        snippetValues={snippetValues}
+                        setSnippetValues={setSnippetValues}
+                    />
+                </StyledInputCol>
+                <Col>
+                    <CopyBlock
+                        text={code}
+                        language={language == 'Choose Language' ? defaultLangauge : language}
+                        showLineNumbers={true}
+                        startingLineNumber={true}
+                        theme={dracula}
+                        wrapLines
+                    />
+                </Col>
             </Row>
         </Container>
     );
 }
+
+const languages = [
+    'abap',
+    'actionscript',
+    'ada',
+    'arduino',
+    'autoit',
+    'c',
+    'clojure',
+    'cs',
+    'cpp',
+    'coffeescript',
+    'csharp',
+    'css',
+    'cuda',
+    'd',
+    'dart',
+    'delphi',
+    'elixir',
+    'elm',
+    'erlang',
+    'fortran',
+    'foxpro',
+    'fsharp',
+    'go',
+    'graphql',
+    'gql',
+    'groovy',
+    'haskell',
+    'haxe',
+    'html',
+    'java',
+    'javascript',
+    'json',
+    'julia',
+    'jsx',
+    'js',
+    'kotlin',
+    'latex',
+    'lisp',
+    'livescript',
+    'lua',
+    'mathematica',
+    'makefile',
+    'matlab',
+    'objectivec',
+    'objective',
+    'objectpascal',
+    'ocaml',
+    'octave',
+    'perl',
+    'php',
+    'powershell',
+    'prolog',
+    'puppet',
+    'python',
+    'qml',
+    'r',
+    'racket',
+    'restructuredtext',
+    'rest',
+    'ruby',
+    'rust',
+    'sass',
+    'less',
+    'scala',
+    'scheme',
+    'shell',
+    'smalltalk',
+    'sql',
+    'standardml',
+    'sml',
+    'swift',
+    'tcl',
+    'tex',
+    'text',
+    'tsx',
+    'ts',
+    'typescript',
+    'vala',
+    'vbnet',
+    'verilog',
+    'vhdl',
+    'xml',
+    'xquery',
+    'yaml'
+]
